f4bf89d7c776ed61e46d50e1d2d63e3a2089c880
hoang <nguyenbahoang02>
2023-09-27 17:19:36 +0700

    test

--- Android/src/main/kotlin/Main.kt
+++ Android/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun EcEc(number: Int){
+    repeat(number){
+        println("éc éc")
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    EcEc(10)
 }
